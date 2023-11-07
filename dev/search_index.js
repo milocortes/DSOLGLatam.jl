e8f5d74bba73a2e1e34f24bdba8fc026bd63e793
Documenter.jl <documenter@juliadocs.github.io>
2023-11-07 02:04:48 +0000

    build based on b31989c

---
+++ dev/search_index.js
@@ -0,0 +1,3 @@
+var documenterSearchIndex = {"docs":
+[{"location":"#DSOLGLatam.jl","page":"DSOLGLatam.jl","title":"DSOLGLatam.jl","text":"","category":"section"},{"location":"","page":"DSOLGLatam.jl","title":"DSOLGLatam.jl","text":"Documentation for DSOLGLatam.jl","category":"page"}]
+}
